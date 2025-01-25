001247d6a50080df40b0ecb3fb5f9510e86a3daa
YuliiaT <115080417+Za1605@users.noreply.github.com>
2025-01-26 00:09:42 +0200

    part

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -102,3 +102,26 @@ for (const product of products) {
 //  - користувачів зі статусом true
 //  - користувачів зі статусом false
 //  - користувачів які старші за 30 років
+
+let users = [
+     {name: 'vasya', age: 31, status: false,},
+     {name: 'petya', age: 30, status: true,},
+     {name: 'kolya', age: 29, status: true,},
+     {name: 'olya', age: 28, status: false,},
+     {name: 'max', age: 30, status: true,},
+     {name: 'anya', age: 31, status: false,},
+     {name: 'oleg', age: 28, status: false,},
+     {name: 'andrey', age: 29, status: true,},
+     {name: 'masha', age: 30, status: true,},
+     {name: 'olya', age: 31, status: false,},
+     {name: 'max', age: 31, status: true,},
+ ];
+
+for (const user of users) {
+    if(user.status){
+        document.write(JSON.stringify(user));
+
+    }
+
+}
+
